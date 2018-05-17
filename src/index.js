a661332d9505e882c0a9e725bba104004c9a5c92
Dubravko <woose@web.de>
2018-05-17 15:51:23 +0200

    für http request, axios und redux-promise installieren und in middleware einbinden

--- src/index.js
+++ src/index.js
@@ -3,45 +3,23 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from "react-router-dom";
-import { FormGroup, ControlLabel, FormControl, Button } from "react-bootstrap";
+import promise from "redux-promise";
 
 import reducers from "./reducers";
-import PostsIndex from "./components/post_index";
+import PostsIndex from "./components/posts_index";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() {
-    return <div>Hello</div>;
-  }
-}
-
-class GoodBye extends React.Component {
-  render() {
-    return <div>GoodBye</div>;
-  }
-}
-
-class Yes extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <Button bsStyle="primary">Drücken</Button>
-      </div>
-    );
-  }
-}
+// redux promise installieren und anschließend in die Middleware einbinden
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        Sacasy
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={GoodBye} />
-        <Route path="/" component={Yes} />
+        <div>Sacasy</div>
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>,
   document.querySelector(".container")
 );
+PostsIndex;
