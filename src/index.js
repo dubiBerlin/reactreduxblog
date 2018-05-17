cfc80bdcd0efed7c62001726f5e7389623b1e078
Dubravko <woose@web.de>
2018-05-17 14:28:29 +0200

    bootstrap btn hinzugefügt in eigener komponente

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from "react-router-dom";
+import { FormGroup, ControlLabel, FormControl, Button } from "react-bootstrap";
 
-import App from "./components/app";
 import reducers from "./reducers";
+import PostsIndex from "./components/post_index";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -21,12 +22,24 @@ class GoodBye extends React.Component {
   }
 }
 
+class Yes extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Button bsStyle="primary">Drücken</Button>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
+        Sacasy
         <Route path="/hello" component={Hello} />
         <Route path="/goodbye" component={GoodBye} />
+        <Route path="/" component={Yes} />
       </div>
     </BrowserRouter>
   </Provider>,
