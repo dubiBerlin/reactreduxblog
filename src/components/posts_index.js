a5d3541eff8cc2e211a52d4a3fdce5648102d8b1
Dubravko <woose@web.de>
2018-05-18 09:54:39 +0200

    Geladene Daten durch mapStateToProps and die Props der Komponente binden2

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,15 +3,22 @@ import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
 
 class PostIndex extends Component {
-  /* wird erst dann ausgeführt nachdem die Komponente am bildschirm
-     angezeigt wird. Hier kann man Daten holen und einfügen. */
+  /* 2. wird erst dann ausgeführt nachdem die Komponente am bildschirm
+        angezeigt wird. Hier kann man Daten holen und einfügen. */
   componentDidMount() {
     this.props.fetchPosts();
   }
 
   render() {
-    return <div>Posts index</div>;
+    console.log(this.props.posts);
+    return <div>PostIndex</div>;
   }
+}
+
+// 3. man bindet den state an die props der Komponente. Die Daten sind da durch fetchPosts()
+//    innerhalb der componentDidMount Methode
+function mapStateToProps(state) {
+  return { posts: state.posts };
 }
 
 // function mapDispatchToProps(dispatch) {
@@ -23,7 +30,7 @@ class PostIndex extends Component {
 //   );
 // }
 
-// null weil nicht "mapStateToProps" übergeben wird
-// Anstelle von mapDispatchToProps kann man auch einfach die Action so übergeben
-export default connect(null, { fetchPosts: fetchPosts })(PostIndex);
+// 1. null weil nicht "mapStateToProps" übergeben wird
+//    Anstelle von mapDispatchToProps kann man auch einfach die Action so übergeben
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostIndex);
 //export default connect(null, mapDispatchToProps)(PostIndex);
