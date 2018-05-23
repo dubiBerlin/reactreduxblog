d82daa844a003d571812f4cdf608e913e976f20c
Dubravko <woose@web.de>
2018-05-23 09:17:25 +0200

    neuen action creator für die neue Komponente erstellt

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
 export const CREATE_POSTS = "create_posts";
+export const FETCH_POST = "fetch_post";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=irgendwas111";
@@ -24,6 +25,16 @@ export function createPost(values, callback) {
 
   return {
     type: CREATE_POSTS,
+    payload: request
+  };
+}
+
+/* Action die einen bestimmten Post holt und id als Paramater bekommt.
+    die id ist die id des Posts. */
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
