b4f1525dce527e0e6ca4d4a0790d98b742517204
Dubravko <woose@web.de>
2018-05-23 09:09:31 +0200

    neue Komponente erstellt und in Reactdom eingefügt

---
+++ src/components/posts_show.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from "../actions";
+
+class PostsShow extends Component {
+  render() {
+    return <div>Posts show!</div>;
+  }
+}
+
+export default PostShow;
