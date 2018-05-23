0aca76ece681db784a515994c0228d88d49082ed
Dubravko <woose@web.de>
2018-05-23 09:25:54 +0200

    reducer für die neue komponente erstellt

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from "../actions";
+import { FETCH_POSTS, FETCH_POST } from "../actions";
 import _ from "lodash";
 // state soll ein Objekt sein
 export default function(state = {}, action) {
@@ -8,6 +8,14 @@ export default function(state = {}, action) {
            Objekten auf. Man übergibt den Paramater den man als key haben möchte (hier: id) */
       return _.mapKeys(action.payload.data, "id");
 
+    case FETCH_POST:
+      // ein einzelner Post ist ein Objekt also gibt er ein einzelnes Objekt zurück.
+      // {... state} => nimm alle states die wir bis jetzt haben und füge sie in das neue Objekt ein
+      const post = action.payload.data;
+      const newState = { ...state };
+      newState[post.id] = post;
+      return newState;
+    // es6 => return {...state, [action.payload.data.id]: action.payload.data}
     default:
       return state;
   }
